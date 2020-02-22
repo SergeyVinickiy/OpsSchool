25513330bf435b7eae1011d24485a7a51f915697
Sergey Vinickiy <svinickiy@yahoo.com>
2020-02-22 16:19:26 +0200

    tasks 4-6

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+chmod 600 ~/.ssh/authorized_keys
+sudo apt-get update
+sudo apt-get install sshpass
+cat /home/vagrant/.ssh/id_rsa.pub | sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@192.168.100.10 "cat >> /home/vagrant/.ssh/authorized_keys && ssh-keyscan -t rsa 192.168.100.11,server2 >> ~/.ssh/known_hosts"
+sshpass -p "vagrant" scp -o StrictHostKeyChecking=no vagrant@192.168.100.10:/home/vagrant/.ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
+cat /home/vagrant/.ssh/id_rsa.pub >>  /home/vagrant/.ssh/authorized_keys
+sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@192.168.100.10 "rm -f /home/vagrant/.ssh/id_rsa.pub"
+ssh-keyscan -t rsa 192.168.100.10,server1 >> /home/vagrant/.ssh/known_hosts
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/
+rm -f /home/vagrant/.ssh/id_rsa.pub
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+HOST=$(hostname)
+server2P="server2"
+server1P="server1"
+
+
+if [ "$HOST" == "$server1P" ] ##note the space after the string you are searching for
+then
+HOST="server2"
+echo $HOST
+
+elif [  "$HOST" == "$server2P" ]
+then
+HOST="server1"
+echo $HOST
+
+else
+    echo "This script is not familiar with this ip. In order to add it, edit the shell script of exercise6-fix and add this ip to the if condition"
+fi
+
+#echo "echo hello world" >>  /home/vagrant/file1.sh
+File_1_path=$1
+Path=$2
+my_array=($File_1_path $Path)
+
+
+#pv -size | scp  ${my_array[0]} server2:${my_array[1]}
+rsync -rt   ${my_array[0]}  $HOST:${my_array[1]} --stats | grep  "Total bytes sent" | sed 's/[^0-9]*//g'
+
+
+if [ -f "exercise6-fix.sh" ]
+then
+echo "file/s allready exist"
+ else
+  echo "if [ /"$HOST/" == /"$server1P/" ]">>  /home/vagrant/exercise6-fix.sh
+fi
